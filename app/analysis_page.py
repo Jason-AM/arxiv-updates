7196442627c1d6290fc7f976d81cc2cff385528c
Jason Myers <jasonmyers@Jasons-MBP-2.cust.communityfibre.co.uk>
2022-06-24 15:42:33 +0100

    adding important dependency and title for second page

--- app/analysis_page.py
+++ app/analysis_page.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
 from noun_chunk import get_word_cloud
-from utils import display_articles, read_todays_data
+from utils import read_todays_data
 
 
 def analysis_page():
     list_of_titles_urls = list(read_todays_data(["cs.LG"]))
 
     # st.pyplot(fig=get_word_cloud(list_of_titles_urls))
+
+    st.markdown(
+        """
+        <h3 style='text-align: center; margin-bottom: -35px;'>
+        Top phrases found
+        </h1>
+    """,
+        unsafe_allow_html=True,
+    )
+
     for word in get_word_cloud(list_of_titles_urls):
         st.text(word)
